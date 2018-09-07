27cbcd2065fe386617671076628b0d8eb1e8030b
Leo espinoza <leo.spinoza@yahoo.com>
2018-09-07 00:05:53 -0500

    added all routes end points and middlewares

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', listsRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+app.use('/lists', listsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
